8633cf0aa618e730e0e83d7dd42506e933f95fb6
Murillo Paula <murillopaula@americashealth.com.br>
2019-04-19 06:23:04 +0000

    tests(upstream-by-header) add schema validation
    Unit tests for the plugin's schema are added in this commit. Validations
    include: headers and upstream semantics; schema structure; required
    fields.
    
    The basic structure for the tests followed the schema validation pattern
    used on Kong-bundled plugins:
    https://github.com/Kong/kong/tree/master/kong/plugins

---
+++ spec/upstream-by-header/02-schema_spec.lua
@@ -0,0 +1,91 @@
+local schema_def = require("kong.plugins.upstream-by-header.schema")
+local v = require("spec.helpers").validate_plugin_config_schema
+
+describe("Plugin: upstream-by-header (schema)", function()
+  it("validates header name", function()
+    local config = {
+      rules = {
+        {
+          headers = {
+            ["X-Region!"] = "Italy",
+          },
+          upstream_name = "italy_cluster",
+        },
+      },
+    }
+    local ok, err = v(config, schema_def)
+    assert.falsy(ok)
+    assert.equal("bad header name 'X-Region!', allowed characters are A-Z, a-z, 0-9, '_', and '-'", err.config.rules[1].headers)
+  end)
+
+  it("validates upstream", function()
+    local config = {
+      rules = {
+        {
+          headers = {
+            ["X-Country"] = "Italy",
+          },
+          upstream_name = "123",
+        },
+      },
+    }
+    local ok, err = v(config, schema_def)
+    assert.falsy(ok)
+    assert.equal("invalid value: 123", err.config.rules[1].upstream_name)
+    --print(require("spec.helpers").intercept(err.config.rules[1].upstream_name))
+  end)
+
+  it("validates schema structure", function()
+    local config = {
+      rules = {
+        {
+          headers = {
+            ["X-Country"] = "Italy"
+          },
+          upstream_name = "italy_cluster"
+        }
+      }
+    }
+
+    local ok, err = v(config, schema_def)
+    assert.truthy(ok)
+    assert.is_nil(err)
+  end)
+
+  it("validates required config object", function()
+    local config = nil
+    local ok, err = v(config, schema_def)
+    assert.falsy(ok)
+  end)
+
+  it("validates required `rules` field", function()
+    local config = {}
+    local ok, err = v(config, schema_def)
+    assert.falsy(ok)
+    assert.equal("required field missing", err.config.rules)
+  end)
+
+  it("`rules` field must have at least one record", function()
+    local config = {
+      rules = {
+      }
+    }
+    local ok, err = v(config, schema_def)
+    assert.falsy(ok)
+    assert.equal("length must be at least 1", err.config.rules)
+  end)
+
+  it("rule record (headers and upstream_name) field missing", function()
+    local config = {
+      rules = {
+        {
+
+        }
+      }
+    }
+    local ok, err = v(config, schema_def)
+    assert.falsy(ok)
+    assert.equal("required field missing", err.config.rules[1].headers)
+    assert.equal("required field missing", err.config.rules[1].upstream_name)
+  end)
+end)

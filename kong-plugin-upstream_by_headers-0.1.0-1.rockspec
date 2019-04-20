guolin302/kong-upstream-by-header

commit 143bcf6868b8491813960cce0c4d63ffb3ea0dd3

chore(upstream-by-header) rename plugin name
This commit adjusts the plugin name from all the necessary places:
rockspec file, unit tests and directories.

Because the plugin was based on this plugin template [0], the
`myplugin` name was inherited along with it.

[0] https://github.com/Kong/kong-plugin

kong-plugin-upstream_by_headers-0.1.0-1.rockspec
@@ -1,4 +1,4 @@
-package = "kong-plugin-myplugin"  -- TODO: rename, must match the info in the filename of this rockspec!
+package = "kong-plugin-upstream_by_headers"  -- TODO: rename, must match the info in the filename of this rockspec!
                                   -- as a convention; stick to the prefix: `kong-plugin-`
 version = "0.1.0-1"               -- TODO: renumber, must match the info in the filename of this rockspec!
 -- The version '0.1.0' is the source code version, the trailing '1' is the version of this rockspec.
@@ -7,7 +7,7 @@ version = "0.1.0-1"               -- TODO: renumber, must match the info in the 
 
 -- TODO: This is the name to set in the Kong configuration `plugins` setting.
 -- Here we extract it from the package name.
-local pluginName = package:match("^kong%-plugin%-(.+)$")  -- "myPlugin"
+local pluginName = package:match("^kong%-plugin%-(.+)$")
 
 supported_platforms = {"linux", "macosx"}
 source = {

spec/myplugin/01-access_spec.lua
@@ -1,111 +1,0 @@
-local helpers = require "spec.helpers"
-local version = require("version").version or require("version")
-
-
-local PLUGIN_NAME = "upstream-by-header"
-local KONG_VERSION = version(select(3, assert(helpers.kong_exec("version"))))
-
-
-for _, strategy in helpers.each_strategy() do
-  describe(PLUGIN_NAME .. ": (access) [#" .. strategy .. "]", function()
-    local client
-
-    lazy_setup(function()
-      local bp, route1
-
-      if KONG_VERSION >= version("0.15.0") then
-        --
-        -- Kong version 0.15.0/1.0.0, new test helpers
-        --
-        local bp = helpers.get_db_utils(strategy, nil, { PLUGIN_NAME })
-
-        local route1 = bp.routes:insert({
-          hosts = { "test1.com" },
-        })
-        bp.plugins:insert {
-          name = PLUGIN_NAME,
-          route = { id = route1.id },
-          config = {},
-        }
-      else
-        --
-        -- Pre Kong version 0.15.0/1.0.0, older test helpers
-        --
-        local bp = helpers.get_db_utils(strategy)
-
-        local route1 = bp.routes:insert({
-          hosts = { "test1.com" },
-        })
-        bp.plugins:insert {
-          name = PLUGIN_NAME,
-          route_id = route1.id,
-          config = {},
-        }
-      end
-
-      -- start kong
-      assert(helpers.start_kong({
-        -- set the strategy
-        database   = strategy,
-        -- use the custom test template to create a local mock server
-        nginx_conf = "spec/fixtures/custom_nginx.template",
-        -- set the config item to make sure our plugin gets loaded
-        plugins = "bundled," .. PLUGIN_NAME,  -- since Kong CE 0.14
-        custom_plugins = PLUGIN_NAME,         -- pre Kong CE 0.14
-      }))
-    end)
-
-    lazy_teardown(function()
-      helpers.stop_kong(nil, true)
-    end)
-
-    before_each(function()
-      client = helpers.proxy_client()
-    end)
-
-    after_each(function()
-      if client then client:close() end
-    end)
-
-
-
-    describe("request", function()
-      it("gets a 'hello-world' header", function()
-        local r = assert(client:send {
-          method = "GET",
-          path = "/request",  -- makes mockbin return the entire request
-          headers = {
-            host = "test1.com"
-          }
-        })
-        -- validate that the request succeeded, response status 200
-        assert.response(r).has.status(200)
-        -- now check the request (as echoed by mockbin) to have the header
-        local header_value = assert.request(r).has.header("hello-world")
-        -- validate the value of that header
-        assert.equal("this is on a request", header_value)
-      end)
-    end)
-
-
-
-    describe("response", function()
-      it("gets a 'bye-world' header", function()
-        local r = assert(client:send {
-          method = "GET",
-          path = "/request",  -- makes mockbin return the entire request
-          headers = {
-            host = "test1.com"
-          }
-        })
-        -- validate that the request succeeded, response status 200
-        assert.response(r).has.status(200)
-        -- now check the response to have the header
-        local header_value = assert.response(r).has.header("bye-world")
-        -- validate the value of that header
-        assert.equal("this is on the response", header_value)
-      end)
-    end)
-
-  end)
-end

commit b36c3087b6e4689164b7733a4d360f4afe4f7d90

fix(upstream-by-header) fix rockspec
This commit fixes the issue where the plugin is not being installed with LuaRocks.

Caused by the commit 143bcf6.

kong-plugin-upstream_by_headers-0.1.0-1.rockspec
@@ -1,4 +1,4 @@
-package = "kong-plugin-upstream_by_headers"  -- TODO: rename, must match the info in the filename of this rockspec!
+package = "kong-plugin-upstream-by-headers"  -- TODO: rename, must match the info in the filename of this rockspec!
                                   -- as a convention; stick to the prefix: `kong-plugin-`
 version = "0.1.0-1"               -- TODO: renumber, must match the info in the filename of this rockspec!
 -- The version '0.1.0' is the source code version, the trailing '1' is the version of this rockspec.